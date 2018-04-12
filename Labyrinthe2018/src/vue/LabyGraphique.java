2592e3ace2fde372a1e33044b8c70b7b723ca99d
Gianni Riccardi <riccardi.gianni@gmail.com>
2018-04-12 11:16:47 +0200

    Version avec menu et labyrinthe

--- Labyrinthe2018/src/vue/LabyGraphique.java
+++ Labyrinthe2018/src/vue/LabyGraphique.java
@@ -13,18 +13,50 @@ import modele.*;
  */
 public class LabyGraphique extends JFrame{
     private JPanel pan ; // panneau
+    private JPanel lab;
+    private JPanel menu; // gère les choix
     private JButton boutons[][]; // matrice de boutons
     
     public LabyGraphique (){ // constructeur
         setTitle ("Mon labyrinthe");
-        setSize (300, 150);
+        setSize (600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pan = new JPanel(); // instancier le panneau 
+        menu = new JPanel(); // menu
+        lab = new JPanel(); // labyrinthe
+        pan.setLayout(new BoxLayout(pan, BoxLayout.LINE_AXIS));
+        pan.add(lab);
+        pan.add(menu);
         getContentPane().add(pan); // ajouter le panneau dans la fenêtre
     }
     
     // Méthode qui affiche la grille du labyrinthe 
     public void affiche(Labyrinthe laby) {
-        pan.setLayout(new GridLayout(laby.getTailleY(), laby.getTailleX())); // mise en forme avec une grille 
+        labInit(laby);
+        // À compléter : 
+        for (int y = 0; y < laby.getTailleY(); y++) {
+            for (int x = 0; x < laby.getTailleX(); x++) {
+                Case c = laby.getCase(y, x);
+                if (c instanceof CaseMur) {
+                    // Mur
+                    boutons[y][x].setText("M");
+                } else {
+                    if (c.getVisited()) {
+                        // Case vide visitée
+                        boutons[y][x].setText("V");
+                    } else {
+                        // Case vide non visitée
+                        boutons[y][x].setText("_");
+                    }
+                }
+            }
+        }
+        // rendre la fenetre visible
+        this.setVisible(true);
+    }
+    
+    public void labInit(Labyrinthe laby) {
+        lab.setLayout(new GridLayout(laby.getTailleY(), laby.getTailleX())); // mise en forme avec une grille 
         boutons = new JButton[laby.getTailleY()][]; // instancier les lignes de la matrice de boutons
         for (int i = 0; i < laby.getTailleY(); i++)
             boutons[i] = new JButton[laby.getTailleX()];// Pour chaque ligne de la matrice, instancier les boutons
@@ -32,25 +64,8 @@ public class LabyGraphique extends JFrame{
         for (int i = 0; i < laby.getTailleY(); i++) {
             for (int j = 0; j < laby.getTailleX(); j++) {
                 boutons[i][j] = new JButton(); // instancier chaque bouton 
-                pan.add(boutons[i][j]);
+                lab.add(boutons[i][j]);
             }
         }
-        // À compléter : 
-        for (int y = 0; y < laby.getTailleY(); y++) {
-            for (int x = 0; x < laby.getTailleX(); x++) {
-                Case c = laby.getCase(y, x);
-                if (c instanceof CaseMur) {
-                    // Mur
-                } else {
-                    if (c.getVisited()) {
-                        // Case vide visitée
-                    } else {
-                        // Case vide non visitée
-                    }
-                }
-            }
-        }
-        // rendre la fenetre visible
-        this.setVisible(true);
     }
 }
